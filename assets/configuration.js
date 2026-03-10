add56dbf6fd0c5f0299770ffd62b855f6227ff47
JekyllChess <satranchess@ogulkoseoglu.com>
2026-03-10 14:45:48 +0300

    Update configuration.js

--- assets/configuration.js
+++ assets/configuration.js
@@ -1,7 +1,4 @@
-/* ================================================================
-     CONFIGURATION
-  ================================================================ */
-
+/* Basic configuration */
   const PIECE_THEME =
     "https://chessboardjs.com/img/chesspieces/wikipedia/{piece}.png";
   const NBSP = "\u00A0";
