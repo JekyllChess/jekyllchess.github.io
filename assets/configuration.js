96d4b8b2e6aee6a8d70e574abcc0bff739457475
JekyllChess <satranchess@ogulkoseoglu.com>
2026-03-10 14:44:54 +0300

    Update and rename jekyllchess-two.js to configuration.js

---
+++ assets/configuration.js
@@ -0,0 +1,7 @@
+/* ================================================================
+     CONFIGURATION
+  ================================================================ */
+
+  const PIECE_THEME =
+    "https://chessboardjs.com/img/chesspieces/wikipedia/{piece}.png";
+  const NBSP = "\u00A0";
